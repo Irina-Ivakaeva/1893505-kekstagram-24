6771096c6cd4bd19fb58d87ce73e5f72b8e16fe8
Irina Ivakaeva <ivakaevadev@gmail.com>
2021-09-27 22:54:29 +0300

    Исправила замечания

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 function getRandomInt(from, to) {
-  let min = from;
-  let max = to;
   if (to <= from) {
-    min = to;
-    max = from;
+    throw new Error ('Параметр \'to\' не может быть меньше или равен параметру \'from\'')
+  } else if (from < 0 || to < 0) {
+    throw new Error ('Введенные параметры должны быть больше или равны нулю')
   }
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (to - from)) + from;
 }
 getRandomInt(15, 90);
 
 function checkLength(line, maxLength) {
-  if (line.length === maxLength) {
-    return true;
-  }
-  return false;
+  return line.length === maxLength;
 }
 checkLength('abc', 3);
