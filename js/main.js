ae7e1388bf4e5cae710cb0f39514fd59c522204f
Irina Ivakaeva <ivakaevadev@gmail.com>
2021-09-27 23:00:31 +0300

    Исправила замечания, добавила ;

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function getRandomInt(from, to) {
   if (to <= from) {
-    throw new Error ('Параметр \'to\' не может быть меньше или равен параметру \'from\'')
+    throw new Error ('Параметр \'to\' не может быть меньше или равен параметру \'from\'');
   } else if (from < 0 || to < 0) {
-    throw new Error ('Введенные параметры должны быть больше или равны нулю')
+    throw new Error ('Введенные параметры должны быть больше или равны нулю');
   }
   return Math.floor(Math.random() * (to - from)) + from;
 }
